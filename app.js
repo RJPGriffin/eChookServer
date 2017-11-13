3b59cc36d40529be0b3fcae1eac87eb35b97ca59
Rowan Griffin <rowangee@gmail.com>
2017-11-13 16:09:15 +0000

    Added json parser and middleware.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 
 var app = express(); //Create express app
+var jsonParser = bodyParser.json();
 app.set('view engine', 'ejs');
 app.listen(3000);
+
+app.post('/post/:id', jsonParser, function(req, res) {
+  console.log(req.params.id + ' posted ' + JSON.stringify(req.body));
+  res.status(200).end();
+
+});
+
+app.use('/post', function(req, res) {
+  //handle post data to /post
+});
 
 app.get('/', function(req, res) {
   res.render('cars.ejs');
