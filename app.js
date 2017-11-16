806c95cfae4cae8d20a15a5ea0d06f5b7c4d5783
Rowan Griffin <rowangee@gmail.com>
2017-11-16 22:12:18 +0000

    playing with ejs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-const socket = require('socket.io');
+//const socket = require('socket.io');
 
 
 // App setup
@@ -9,11 +9,11 @@ var app = express();
 var jsonParser = bodyParser.json();
 app.set('view engine', 'ejs');
 
-var server = app.listen(4000, function() {
+var server = app.listen(3000, function() {
   console.log('listening for requests on port 4000,');
 });
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // app.post('/post/:id', jsonParser, function(req, res) {
 //   console.log(req.params.id + ' posted ' + JSON.stringify(req.body));
@@ -21,10 +21,25 @@ app.use(express.static('public'));
 //
 // });
 
-// Socket setup & pass server
-var io = socket(server);
-io.on('connection', (socket) => {
+var carData = {
+  cars: [{
+    name: 'car1',
+    number: '1'
+  }, {
+    name: 'car2',
+    number: '2'
+  }]
+}
 
-  console.log('made socket connection', socket.id);
+app.get('/', function(req, res) {
+  res.render('cars', carData);
 
 });
+
+// Socket setup & pass server
+// var io = socket(server);
+// io.on('connection', (socket) => {
+//
+//   console.log('made socket connection', socket.id);
+//
+// });
