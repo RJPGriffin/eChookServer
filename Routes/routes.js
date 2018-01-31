ba852332069fe1d2b363aeb312f428d90f6ca260
Rowan Griffin <rowangee@gmail.com>
2018-01-31 15:46:54 +0000

    fixed api>getId

--- Routes/routes.js
+++ Routes/routes.js
@@ -161,160 +161,148 @@ router.post('/api/send/:id', jsonParser, function(req, res) {
 
 // Used to get the database ID of a car - for app to send data
 router.post('/api/getid', jsonParser, function(req, res) {
-      console.log('Request to get ID for passcode: ' + req.body.username);
+  console.log('Request to get ID for passcode: ' + req.body.username);
 
-      Cars.findOne({
-          'car': req.body.username
-        }, function(err, car) {
-          // if there are any errors, return the error before anything else
-          if (err)
-            console.log(err);
-          res.send('Undefined Error');
-          res.status(204).end();
-
-          // if no user is found, return the message
-          if (!car) {
-            res.send('Car not found');
-            res.status(204).end();
-          } else if (!car.validPassword(req.body.password)) {
-            res.send('Incorrect Password');
-            res.status(204).end();
-          } else {
-            //Car found, Password Incorrect
-            console.log('Car found: ' + car.id);
-            res.send({
-              'id': car.id
-            });
-            res.status(200).end();
-          }
-
-          // all is well, return successful user
-          return done(null, car);
-
-        }
-        /*  Cars.findOne({ //Old method, only checks for a passcode, and not a hashed one
-            password: req.body.password
-          }).then(function(car) {
-            if (car === null) {
-              res.send('Password Not Recognised');
-              res.status(204).end();
-            } else {
-              console.log('id found: ' + car._id);
-              res.send({
-                'id': car._id
-              });
-              res.status(200).end();
-            }
-          });*/
-      });
-
-    function isLoggedIn(req, res, next) {
-
-      // if user is authenticated in the session, carry on
-      if (req.isAuthenticated())
-        return next();
-
-      // if they aren't redirect them to the home page
-      res.redirect('/');
+  Cars.findOne({
+    'car': req.body.username
+  }, function(err, car) {
+    // if there are any errors, return the error before anything else
+    if (err) {
+      console.log(err);
+      res.send('Undefined Error');
+      res.status(204).end();
     }
 
-    module.exports = router;
-
-    //==========================================================================
-    // Data Functions
-    function updateData(key, dataIn) {
-      console.log('Calling Update Data');
-
-      if (key in dataStore) {
-
-      } else {
-        console.log('Adding ' + key + ' to Data Store');
-        dataStore[key] = dataTemplate;
-        console.log(dataStore);
-      }
-
-      // console.log(dataStore);
-
-      if ('Vt' in dataIn) {
-        dataStore[key].voltage = dataIn.Vt
-      }
-      if ('A' in dataIn) {
-        dataStore[key].current = dataIn.A
-      }
-      if ('V1' in dataIn) {
-        dataStore[key].voltageLower = dataIn.V1
-      }
-      if ('RPM' in dataIn) {
-        dataStore[key].rpm = dataIn.RPM
-      }
-      if ('Spd' in dataIn) {
-        dataStore[key].speed = dataIn.Spd
-      }
-      if ('Thrtl' in dataIn) {
-        dataStore[key].throttle = dataIn.Thrtl
-      }
-      if ('Tmp1' in dataIn) {
-        dataStore[key].temp1 = dataIn.Tmp1
-      }
-      if ('Tmp2' in dataIn) {
-        dataStore[key].temp2 = dataIn.Tmp2
-      }
-      if ('AH' in dataIn) {
-        dataStore[key].ampH = dataIn.AH
-      }
-      if ('Lap' in dataIn) {
-        dataStore[key].currLap = dataIn.Lap
-      }
-      if ('Gear' in dataIn) {
-        dataStore[key].gear = dataIn.Gear
-      }
-      if ('brk' in dataIn) {
-        dataStore[key].brake = dataIn.brk
-      }
-      if ('Lon' in dataIn) {
-        dataStore[key].lon = dataIn.Lon
-      }
-      if ('Lat' in dataIn) {
-        dataStore[key].lat = dataIn.Lat
-      }
-      if ('LL_Time' in dataIn) {
-        dataStore[key].LL_Time = dataIn.LL_Time
-      }
-      if ('LL_V' in dataIn) {
-        dataStore[key].LL_V = dataIn.LL_V
-      }
-      if ('LL_I' in dataIn) {
-        dataStore[key].LL_I = dataIn.LL_I
-      }
-      if ('LL_RPM' in dataIn) {
-        dataStore[key].LL_RPM = dataIn.LL_RPM
-      }
-      if ('LL_Spd' in dataIn) {
-        dataStore[key].LL_Spd = dataIn.LL_Spd
-      }
-      if ('LL_Ah' in dataIn) {
-        dataStore[key].LL_Ah = dataIn.LL_Ah
-      }
-      dataStore[key].updated = Date.now()
-      dataStore[key].status = 'Live'
-
-      console.log(dataStore);
-
+    // if no user is found, return the message
+    if (!car) {
+      console.log("Car Not Found");
+      res.send('Car not found');
+      res.status(204).end();
+    } else if (!car.validPassword(req.body.password)) {
+      console.log("Invalid Password");
+      res.send('Incorrect Password');
+      res.status(204).end();
+    } else {
+      //Car found, Password Incorrect
+      console.log('Car found: ' + car.id);
+      res.send({
+        'id': car.id
+      });
+      res.status(200).end();
     }
 
+  })
 
-    function manageData() {
-      // console.log("Running manageData");
-      Object.keys(dataStore).forEach(function(key) {
-        // console.log('Checking ' + n + n.updated);
-        // console.log('In Loop');
-        if (Date.now() - dataStore[key].updated > 30000) { //30 seconds
-          // console.log('Deleting: ' + dataStore[key]);
-          delete dataStore[key];
-        } else if (Date.now() - dataStore[key].updated > 10000) {
-          // console.log('Marking ' + dataStore[key] + ' Stale');
-          dataStore[key].status = 'Stale'
-        }
-      });
-    };
-    // setInterval(manageData, 10000);
+
+});
+
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
+
+module.exports = router;
+
+//==========================================================================
+// Data Functions
+function updateData(key, dataIn) {
+  console.log('Calling Update Data');
+
+  if (key in dataStore) {
+
+  } else {
+    console.log('Adding ' + key + ' to Data Store');
+    dataStore[key] = dataTemplate;
+    console.log(dataStore);
+  }
+
+  // console.log(dataStore);
+
+  if ('Vt' in dataIn) {
+    dataStore[key].voltage = dataIn.Vt
+  }
+  if ('A' in dataIn) {
+    dataStore[key].current = dataIn.A
+  }
+  if ('V1' in dataIn) {
+    dataStore[key].voltageLower = dataIn.V1
+  }
+  if ('RPM' in dataIn) {
+    dataStore[key].rpm = dataIn.RPM
+  }
+  if ('Spd' in dataIn) {
+    dataStore[key].speed = dataIn.Spd
+  }
+  if ('Thrtl' in dataIn) {
+    dataStore[key].throttle = dataIn.Thrtl
+  }
+  if ('Tmp1' in dataIn) {
+    dataStore[key].temp1 = dataIn.Tmp1
+  }
+  if ('Tmp2' in dataIn) {
+    dataStore[key].temp2 = dataIn.Tmp2
+  }
+  if ('AH' in dataIn) {
+    dataStore[key].ampH = dataIn.AH
+  }
+  if ('Lap' in dataIn) {
+    dataStore[key].currLap = dataIn.Lap
+  }
+  if ('Gear' in dataIn) {
+    dataStore[key].gear = dataIn.Gear
+  }
+  if ('brk' in dataIn) {
+    dataStore[key].brake = dataIn.brk
+  }
+  if ('Lon' in dataIn) {
+    dataStore[key].lon = dataIn.Lon
+  }
+  if ('Lat' in dataIn) {
+    dataStore[key].lat = dataIn.Lat
+  }
+  if ('LL_Time' in dataIn) {
+    dataStore[key].LL_Time = dataIn.LL_Time
+  }
+  if ('LL_V' in dataIn) {
+    dataStore[key].LL_V = dataIn.LL_V
+  }
+  if ('LL_I' in dataIn) {
+    dataStore[key].LL_I = dataIn.LL_I
+  }
+  if ('LL_RPM' in dataIn) {
+    dataStore[key].LL_RPM = dataIn.LL_RPM
+  }
+  if ('LL_Spd' in dataIn) {
+    dataStore[key].LL_Spd = dataIn.LL_Spd
+  }
+  if ('LL_Ah' in dataIn) {
+    dataStore[key].LL_Ah = dataIn.LL_Ah
+  }
+  dataStore[key].updated = Date.now()
+  dataStore[key].status = 'Live'
+
+  console.log(dataStore);
+
+}
+
+
+function manageData() {
+  // console.log("Running manageData");
+  Object.keys(dataStore).forEach(function(key) {
+    // console.log('Checking ' + n + n.updated);
+    // console.log('In Loop');
+    if (Date.now() - dataStore[key].updated > 30000) { //30 seconds
+      // console.log('Deleting: ' + dataStore[key]);
+      delete dataStore[key];
+    } else if (Date.now() - dataStore[key].updated > 10000) {
+      // console.log('Marking ' + dataStore[key] + ' Stale');
+      dataStore[key].status = 'Stale'
+    }
+  });
+};
+// setInterval(manageData, 10000);
