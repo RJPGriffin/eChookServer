3ed7c71a1a8b43606babe0330c7d4cb59b2512f6
Rowan Griffin <rowangee@gmail.com>
2018-03-29 22:20:41 +0100

    fixing track

--- modules/tracks.js
+++ modules/tracks.js
@@ -1,5 +1,8 @@
 var tracks = {
 
+  test: function() {
+    return ('tracks here!');
+  },
   /*
   'name': {
     'latMax': ,
@@ -14,19 +17,19 @@ var tracks = {
       'latMax': 52.393066,
       'latMin': 52.392188,
       'lonMax': -1.438836,
-      'lonMin': -1.438002,
+      'lonMin': -1.438002
     },
     'Rockingham': {
       'latMax': 52.5179712,
       'latMin': 52.512264,
       'lonMax': -0.649619,
-      'lonMin': -0.6661527,
+      'lonMin': -0.6661527
     },
     'Goodwood': {
       'latMax': 50.864790,
       'latMin': 50.853663,
       'lonMax': -0.750720,
-      'lonMin': -0.769699,
+      'lonMin': -0.769699
     }
   },
 
@@ -34,17 +37,19 @@ var tracks = {
     console.log('Running get Track');
     let track = "none";
 
-    Object.keys(this.tracklist).forEach(function(key) {
+    Object.keys(this.trackList).forEach(function(key) {
       //  if (track === "") {
+      console.log('Got key:' + key);
+      console.log('Which Contains:' + JSON.stringify(this.trackList[key]));
       if (lat > this.trackList[key].latMax && lat < this.trackList[key].latMin) {
         if (lon > this.trackList[key].lonMax && lon < this.trackList[key].lonMin) {
           track = key;
         }
       }
       //  }
-      console.log('Found Track:' + track);
-      return (track);
     });
+    console.log('Found Track:' + track);
+    return (track);
   }
 
 };
