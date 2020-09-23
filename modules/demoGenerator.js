7816809477e0bf9739f16a8be991887fff0cbcc6
Rowan Griffin <rowangee@gmail.com>
2020-09-23 09:04:56 +0100

    Added variables to demo generator

--- modules/demoGenerator.js
+++ modules/demoGenerator.js
@@ -14,16 +14,22 @@ demo = {
     var lat = ((Math.random() / 100) + 50.8519424).toFixed(7);
     var lon = ((Math.random() / 100) + -0.7623057).toFixed(7);
     var lap = Math.random() * 10;
+    var temp1 = (Math.random()* 10 + 10).toFixed(1);
+    var temp2 = (Math.random()* 10 + 10).toFixed(1);
+    var throttle = Math.floor(100 - Math.random()*2);
     var track = tracks.getTrack(lat, lon);
     return ({
       'voltage': voltage,
       'voltsLower': voltageLower,
       'current': current,
       'speed': speed,
+      'throttle': throttle,
       'rpm': rpm,
       'time': time,
       'lat': lat,
       'lon': lon,
+      'temp1': temp1,
+      'temp2': temp2,
       'track': track,
       'currLap': lap
 
